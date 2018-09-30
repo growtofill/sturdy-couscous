7459b789c53a13b655860cce912126caedb8edd7
Pavlo Mykhalov <pmykhalov@gmail.com>
2018-09-30 13:20:27 +0300

    Fetch from data url

--- src/App.js
+++ src/App.js
@@ -11,15 +11,30 @@ import Filters from "./components/Filters/Filters";
 import CrewMemberList from "./components/CrewMemberList/CrewMemberList";
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?nat=gb&results=5')
+      .then(response => response.json())
+      .then(json => this.setState({ crewMembers: json.results }))
+  }
+
   render() {
     return (
       <StateProvider>
         <Filters />
-        <div className="Row">
-          <div className="Col"><CrewMemberList stage="applied" /></div>
-          <div className="Col"><CrewMemberList stage="interviewing" /></div>
-          <div className="Col"><CrewMemberList stage="hired" /></div>
-        </div>
+          {this.state.crewMembers
+            ? (
+              <div className="Row">
+                <div className="Col">
+                  <CrewMemberList stage="applied" crewMembers={this.state.crewMembers} /></div>
+                <div className="Col">
+                  <CrewMemberList stage="interviewing" crewMembers={this.state.crewMembers} /></div>
+                <div className="Col">
+                  <CrewMemberList stage="hired" crewMembers={this.state.crewMembers} />
+                </div>
+              </div>
+            ) : "..."}
       </StateProvider>
     )
   }
